e5a394d9f3ebc41c18b6d77cb08ee72b6bd1bdb8
Chaitanyakrishnappagari <chaitanya6502@gmail.com>
2023-10-17 04:58:46 +0000

    this is the first commit for the django project

---
+++ string.java
@@ -0,0 +1,12 @@
+class Main {
+  public static void main(String[] args)  {
+
+  // create a string 
+  string greet = "Hello! World";
+  System.out.println("String: " + greet);
+
+  // get the length of greet
+  int length = great.length();
+  System.out.println("Length; " + length);
+  }
+ } 
